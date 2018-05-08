301618545d15f88322966f180f454dbfe45dd82c
DmitriyIvanko <dmitriy.ivanko@yumasoft.com>
2018-05-08 20:59:08 +0300

    + create api;

---
+++ test-webapi/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace test_webapi.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [ActionName("sign-in")]
+        public int GetPing()
+        {
+            var a = 5;
+            return a;
+        }
+    }
+}
---
+++ test-webapi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="test_webapi.WebApiApplication" Language="C#" %>
