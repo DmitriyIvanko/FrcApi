b1aa8c5b13838d9e30c083c160928c20116d6763
DmitriyIvanko <dmitriy.ivanko@yumasoft.com>
2018-05-08 21:56:58 +0300

    + add simple connection to database;

---
+++ test-webapi/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test_webapi.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+}
